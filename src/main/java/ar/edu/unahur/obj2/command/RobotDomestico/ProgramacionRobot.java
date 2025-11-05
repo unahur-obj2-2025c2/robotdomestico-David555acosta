d295f7ed581032ac5a0405920b28f54e033dc23e
David Acosta <davidisrraelacostaz.9@gmail.com>
2025-11-05 17:55:50 -0300

    preparando estructura general

---
+++ src/main/java/ar/edu/unahur/obj2/command/RobotDomestico/ProgramacionRobot.java
@@ -0,0 +1,8 @@
+package ar.edu.unahur.obj2.command.RobotDomestico;
+
+public interface ProgramacionRobot {
+
+    void cargar(Double tiempo);
+
+    Integer tiempoDeTareas();
+}
---
+++ src/main/java/ar/edu/unahur/obj2/command/RobotDomestico/Robot.java
@@ -0,0 +1,5 @@
+package ar.edu.unahur.obj2.command.RobotDomestico;
+
+public class Robot {
+    
+}
